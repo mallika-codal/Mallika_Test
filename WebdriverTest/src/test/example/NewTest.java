1b0b401fc516171b7d311013af41579f91c9882d
malli <malli@host.docker.internal>
2020-09-21 20:47:48 +0530

    test

--- WebdriverTest/src/test/example/NewTest.java
+++ WebdriverTest/src/test/example/NewTest.java
@@ -16,7 +16,7 @@ public class NewTest {
 	public void testEasy() {	
 		driver.get("http://demo.guru99.com/test/guru99home/");  
 		String title = driver.getTitle();				 
-		Assert.assertTrue(title.contains("Demo Guru99 Page view")); 		
+		Assert.assertTrue(title.contains("Demo Guru99 Page view test")); 		
 	}	
 	@BeforeTest
 	public void beforeTest() {	
